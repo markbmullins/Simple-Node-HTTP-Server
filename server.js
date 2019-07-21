723fdd7a30314f1679b3108eadf4f10c2150ada2
markbmullins <markmullins95@gmail.com>
2019-07-21 10:57:05 -0400

    Finished server and added some simple CSS

--- server.js
+++ server.js
@@ -3,52 +3,55 @@ const http = require('http');
 const path = require('path');
 
 const SERVER_PORT = process.env.PORT || 5001;
-const STATUS_CODE = {
-    "OK": 200,
-    "BAD_REQUEST": 400,
-    "INTERNAL_SERVER_ERROR": 500,
-};
 
 const server = http.createServer((request, response) => {
+  // Route request to root or requested page
+  const simpleRouter = request.url === '/' ? 'index.html' : request.url;
+  let filePath = path.join(__dirname, 'pages', simpleRouter);
 
-    // Route request to root or requested page
-    const simpleRouter = request.url === "/" ? "index.html" : request.url;
-    let filePath = path.join(__dirname, "pages", simpleRouter);
+  const fileExtention = path.extname(filePath);
 
-    const fileExtention = path.extName(filePath);
+  let contentType = 'text/html';
+  switch (fileExtention) {
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+    case '.css':
+      contentType = 'text/css';
+      break;
+    case '.json':
+      contentType = 'application/json';
+      break;
+  }
 
-    let contentType = '';
-    switch (fileExtention) {
-        case ".js":
-            contentType = "text/javascript";
-            break;
-        case ".css":
-            contentType = "text/css";
-            break;
-        case ".json":
-            contentType = "application/json";
-            break;
-        default:
-            contentType = "text/html";
+  if (fileExtention == '' && contentType == 'text/html') {
+    filePath += '.html';
+  }
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code == 'ENOENT') {
+        fs.readFile(
+          path.join(__dirname, 'pages', '404.html'),
+          (err, content) => {
+            if (err) {
+              console.log("Can't load 404 Page");
+              response.end();
+            }
+            response.writeHead(200, { 'Content-Type': 'text/html' });
+            response.end(content, 'utf8');
+          }
+        );
+      } else {
+        path.join(__dirname, 'pages', '404.html');
+      }
+    } else {
+      response.writeHead(200, { 'Content-Type': contentType });
+      response.end(content, 'utf8');
     }
-
-    if (fileExtention == "" && contentType == "text/html") {
-        filePath += ".html";
-    }
-
-    fs.readFile(filePath, (err, content) => {
-        if (!err) {
-            response.writeHead(STATUS_CODE.OK, { "Content-Type": "text/html" });
-            response.end(content, "utf8");
-        }
-        if (err.code === "ENOENT") {
-
-        } else {
-            path.join(__dirname, "pages", "404.html");
-
-        }
-    });
-
+  });
 });
 
-server.listen(SERVER_PORT, () => console.log(`Starting server on port ${SERVER_PORT}`))
+server.listen(SERVER_PORT, () =>
+  console.log(`Starting server on port ${SERVER_PORT}`)
+);
